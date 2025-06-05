e710cb8e82ff639f1a5127836ab63a425dc7d2c4
clayne <clayne@users.noreply.github.com>
2025-06-05 10:07:54 +0000

    deploy: 3367c3a005ac3d8659effb0858526931f68fc434

---
+++ navtreedata.js
@@ -0,0 +1,101 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "FTXUI", "index.html", [
+    [ "Introduction", "index.html", "index" ],
+    [ "Getting Started", "getting-started.html", [
+      [ "Install FTXUI", "getting-started.html#install-ftxui", null ],
+      [ "Minimal Example", "getting-started.html#minimal-example", null ],
+      [ "Starter Template", "getting-started.html#starter-template", null ]
+    ] ],
+    [ "Installation", "installation.html", "installation" ],
+    [ "ftxui", "ftxui.html", [
+      [ "ftxui/screen", "ftxui.html#ftxuiscreen", null ],
+      [ "ftxui/dom", "ftxui.html#ftxuidom", null ],
+      [ "ftxui/component", "ftxui.html#ftxuicomponent", null ]
+    ] ],
+    [ "ftxui / screen", "module-screen.html", [
+      [ "Screen", "module-screen.html#ftxuiscreen-1", null ],
+      [ "Dimension", "module-screen.html#ftxuidimension", null ],
+      [ "Pixel", "module-screen.html#ftxuipixel", null ],
+      [ "Color", "module-screen.html#ftxuicolor", null ]
+    ] ],
+    [ "ftxui / dom", "module-dom.html", "module-dom" ],
+    [ "ftxui / component", "module-component.html", "module-component" ],
+    [ "C++20 Modules", "cpp20-modules.html", null ],
+    [ "Reference", "topics.html", "topics" ],
+    [ "Modules", "modules.html", [
+      [ "Modules List", "modules.html", "modules_dup" ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", [
+      [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+      [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+      [ "Variables", "namespacemembers_vars.html", null ],
+      [ "Typedefs", "namespacemembers_type.html", null ],
+      [ "Enumerations", "namespacemembers_enum.html", null ],
+      [ "Enumerator", "namespacemembers_eval.html", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", "functions_eval" ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"animation_8cpp.html",
+"classftxui_1_1ScreenInteractive.html#a0882d58bb8ca1be9953135338456cf9e",
+"examples_2component_2button_8cpp.html",
+"group__component.html#a03bfec859eac87be20f8952c1eb89de0a9d4d8b0b72fc2659da772d761a3c5ecb",
+"group__dom.html#ga751593eb3bc2e704eb6667ed518f614e",
+"group__screen.html#abd07c48f62cc507fafa2486a43130fcfa8e2c9e620d6245638b27d9c1fa2d0545",
+"menu__style_8cpp.html#a62669e7b3cf3a388a714910a16cce19b",
+"screen__interactive_8hpp_source.html",
+"structftxui_1_1ColorInfo.html#a4769b22557e2bcd3edbc69f2caaf691f",
+"terminal_8hpp.html#ab87bacfdad76e61b9412d7124be44c1c"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/files_8.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['image_2ecpp_0',['image.cpp',['../image_8cpp.html',1,'']]],
+  ['image_2ecppm_1',['image.cppm',['../image_8cppm.html',1,'']]],
+  ['image_2ehpp_2',['image.hpp',['../image_8hpp.html',1,'']]],
+  ['input_5fin_5fframe_2ecpp_3',['input_in_frame.cpp',['../input__in__frame_8cpp.html',1,'']]],
+  ['input_5fstyle_2ecpp_4',['input_style.cpp',['../input__style_8cpp.html',1,'']]],
+  ['installation_2emd_5',['installation.md',['../installation_8md.html',1,'']]],
+  ['installation_5fbazel_2emd_6',['installation_bazel.md',['../installation__bazel_8md.html',1,'']]],
+  ['installation_5fcmake_2emd_7',['installation_cmake.md',['../installation__cmake_8md.html',1,'']]],
+  ['installation_5fconan_2emd_8',['installation_conan.md',['../installation__conan_8md.html',1,'']]],
+  ['installation_5fvcpkg_2emd_9',['installation_vcpkg.md',['../installation__vcpkg_8md.html',1,'']]],
+  ['introduction_2emd_10',['introduction.md',['../introduction_8md.html',1,'']]],
+  ['inverted_2ecpp_11',['inverted.cpp',['../inverted_8cpp.html',1,'']]],
+  ['italic_2ecpp_12',['italic.cpp',['../italic_8cpp.html',1,'']]]
+];
